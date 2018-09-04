5d50ce9ae58a4cc413e0b13ebf6a61c691b1de4f
annaschelin87 <annaschelin87@gmail.com>
2018-09-04 11:42:50 +0200

    DBConnection

---
+++ FindNearestChangingTableVersion1/Data/NewHorizonsDBContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FindNearestChangingTableVersion1.Data
+{
+    public class NewHorizonsDBContext : DbContext
+    {
+        public NewHorizonsDBContext()
+        {
+        }
+
+        public NewHorizonsDBContext(DbContextOptions<NewHorizonsDBContext> options)
+            : base(options)
+        {
+        }
+    }
+}
--- FindNearestChangingTableVersion1/Startup.cs
+++ FindNearestChangingTableVersion1/Startup.cs
@@ -29,6 +29,9 @@ namespace FindNearestChangingTableVersion1
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
 
+            services.AddDbContext<NewHorizonsDBContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("NewHorizonsConnection")));
+
             services.AddIdentity<ApplicationUser, IdentityRole>()
                 .AddEntityFrameworkStores<ApplicationDbContext>()
                 .AddDefaultTokenProviders();
