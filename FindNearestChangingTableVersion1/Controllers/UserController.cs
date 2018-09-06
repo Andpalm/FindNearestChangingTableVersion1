99f7547534647265c2656ba267ce6d263bf74510
annaschelin87 <annaschelin87@gmail.com>
2018-09-06 09:00:40 +0200

    !

---
+++ FindNearestChangingTableVersion1/Controllers/UserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FindNearestChangingTableVersion1.Data;
+using FindNearestChangingTableVersion1.Models;
+using FindNearestChangingTableVersion1.Models.UserViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FindNearestChangingTableVersion1.Controllers
+{
+    public class UserController : Controller
+    {
+        private NewHorizonsDBContext context;
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public UserController(NewHorizonsDBContext context, UserManager<ApplicationUser> userManager)
+        {
+            this.context = context;
+            this.userManager = userManager;
+        }
+        public IActionResult EditLocation()
+        {
+            List<EditLocationViewModel> locationList = EditLocationViewModel.DisplayList(context); 
+            return View();
+        }
+    }
+}
---
+++ FindNearestChangingTableVersion1/Models/UserViewModels/EditLocationViewModel.cs
@@ -0,0 +1,25 @@
+﻿using FindNearestChangingTableVersion1.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FindNearestChangingTableVersion1.Models.UserViewModels
+{
+    public class EditLocationViewModel
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Adress { get; set; }
+        public string UserID { get; set; }
+        public string Description { get; set; }
+        public string Hours { get; set; }
+        public string LatLng { get; set; }
+
+        internal static List<EditLocationViewModel> DisplayList(NewHorizonsDBContext context)
+        {
+            List<EditLocationViewModel> list = new List<EditLocationViewModel>();
+            return list;
+        }
+    }
+}
