cdd3f2b7b95b274fe73eb2e1b612c522eadcc151
annaschelin87 <annaschelin87@gmail.com>
2018-09-10 16:07:09 +0200

    changes

--- FindNearestChangingTableVersion1/Startup.cs
+++ FindNearestChangingTableVersion1/Startup.cs
@@ -42,7 +42,7 @@ namespace FindNearestChangingTableVersion1
                 // Password settings.
                 options.Password.RequireDigit = true;
                 options.Password.RequireLowercase = true;
-                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireNonAlphanumeric = false;
                 options.Password.RequireUppercase = true;
                 options.Password.RequiredLength = 6;
                 options.Password.RequiredUniqueChars = 1;
